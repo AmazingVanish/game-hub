8a2a33e9b79c15e78dfcf7aefc40d4ca861a0fb7
Chris Van Cleve <chris@vanclevefamily.net>
2023-07-16 19:48:53 -0500

    Building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,36 +1,37 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
 
-interface Game {
-  id: number;
-  name: string;
+export interface Game {
+    id: number;
+    name: string;
+    background_image: string;
 }
 
 interface FetchGamesResponse {
-  count: number;
-  results: Game[];
+    count: number;
+    results: Game[];
 }
 
 const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState('');
 
-  useEffect(() => {
-    const controller = new AbortController();
+    useEffect(() => {
+        const controller = new AbortController();
 
-    apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((response) => setGames(response.data.results))
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-      });
+        apiClient
+            .get<FetchGamesResponse>('/games', { signal: controller.signal })
+            .then((response) => setGames(response.data.results))
+            .catch((error) => {
+                if (error instanceof CanceledError) return;
+                setError(error.message);
+            });
 
-    return () => controller.abort();
-  }, []);
+        return () => controller.abort();
+    }, []);
 
-  return { games, error };
+    return { games, error };
 };
 
 export default useGames;
