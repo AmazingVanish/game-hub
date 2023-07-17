c3667914d71e36d0dffd2db009bfcc27a5b0700f
Chris Van Cleve <chris@vanclevefamily.net>
2023-07-17 11:52:01 -0500

    Refactor: Extract a query object

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,25 +4,42 @@ import getCroppedImageUrl from '../services/image-url';
 import GenreListSkeleton from './GenreListSkeleton';
 
 interface GenreListProps {
-    onSelecteGenre: (genre: Genre) => void;
+    onSelectedGenre: (genre: Genre) => void;
     selectedGenre: Genre | null;
 }
 
-const GenreList = ({ selectedGenre, onSelecteGenre }: GenreListProps) => {
+const GenreList = ({ selectedGenre, onSelectedGenre }: GenreListProps) => {
     const { data, isLoading, error } = useGenres();
-    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 156, 17, 18, 19];
+    const skeletons = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 156, 17, 18, 19,
+    ];
 
     if (error) return null;
 
     return (
         <List>
-            {isLoading && skeletons.map((skeleton) => <GenreListSkeleton key={skeleton} />)}
+            {isLoading &&
+                skeletons.map((skeleton) => (
+                    <GenreListSkeleton key={skeleton} />
+                ))}
             {data.map((genre) => (
                 <ListItem key={genre.id} paddingY="5px">
-                    <Card bg={genre.id === selectedGenre?.id ? 'blue.500' : ''} borderRadius={8} margin="3px">
+                    <Card
+                        bg={genre.id === selectedGenre?.id ? 'blue.500' : ''}
+                        borderRadius={8}
+                        margin="3px"
+                    >
                         <HStack>
-                            <Image boxSize="64px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                            <Button onClick={() => onSelecteGenre(genre)} fontSize="lg" variant="link">
+                            <Image
+                                boxSize="64px"
+                                borderRadius={8}
+                                src={getCroppedImageUrl(genre.image_background)}
+                            />
+                            <Button
+                                onClick={() => onSelectedGenre(genre)}
+                                fontSize="lg"
+                                variant="link"
+                            >
                                 {genre.name}
                             </Button>
                         </HStack>
