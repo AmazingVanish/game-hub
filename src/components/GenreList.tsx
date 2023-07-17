3db306223f0bfe096a0316f5c36e22f20713e11a
Chris Van Cleve <chris@vanclevefamily.net>
2023-07-16 23:48:04 -0500

    Highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,14 @@
 import { Button, Card, HStack, Image, List, ListItem } from '@chakra-ui/react';
-import useGenres from '../hooks/useGenres';
+import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 import GenreListSkeleton from './GenreListSkeleton';
 
 interface GenreListProps {
     onSelecteGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelecteGenre }: GenreListProps) => {
+const GenreList = ({ selectedGenre, onSelecteGenre }: GenreListProps) => {
     const { data, isLoading, error } = useGenres();
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 156, 17, 18, 19];
 
@@ -18,7 +19,7 @@ const GenreList = ({ onSelecteGenre }: GenreListProps) => {
             {isLoading && skeletons.map((skeleton) => <GenreListSkeleton key={skeleton} />)}
             {data.map((genre) => (
                 <ListItem key={genre.id} paddingY="5px">
-                    <Card borderRadius={8} margin="3px">
+                    <Card bg={genre.id === selectedGenre?.id ? 'blue.500' : ''} borderRadius={8} margin="3px">
                         <HStack>
                             <Image boxSize="64px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
                             <Button onClick={() => onSelecteGenre(genre)} fontSize="lg" variant="link">
